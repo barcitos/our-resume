57b3fbd6968fb2ca392cc841dfc33a0622b389ce
Patrik Šlárka <patrik@slarka.com>
2026-07-28 19:38:54 +0200

    Disable Dark Reader by default

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [analyzer(), react()],
 })
